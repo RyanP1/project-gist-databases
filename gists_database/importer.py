8c679043c922908d65a2fe104530bd453f642aed
Peter Ryan <peter.e.ryan@gmail.com>
2018-06-26 18:25:40 +0000

    gists database completed

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,35 @@
 import requests
 
+
 def import_gists_to_database(db, username, commit=True):
-    pass
+    
+    if not commit:
+        pass
+    
+    git_url = "https://api.github.com/users/{}/gists".format(username) 
+    response = requests.get(git_url)
+    
+    response.raise_for_status()
+    user_gists = response.json()
+    
+    for gist in user_gists:
+        params = {
+            'github_id': gist['id'], 
+            'html_url': gist['html_url'], 
+            'git_pull_url': gist['git_pull_url'],
+            'git_push_url': gist['git_push_url'],
+            'commits_url': gist['commits_url'], 
+            'forks_url': gist['forks_url'], 
+            'public': gist['public'], 
+            'created_at': gist['created_at'], 
+            'updated_at': gist['updated_at'], 
+            'comments': gist['comments'], 
+            'comments_url': gist['comments_url']
+        }
+        
+        cursor = db.execute("""INSERT INTO gists (github_id, html_url, 
+        git_pull_url, git_push_url, commits_url, forks_url, public, created_at, 
+        updated_at, comments, comments_url) VALUES (:github_id, :html_url, 
+        :git_pull_url, :git_push_url, :commits_url, :forks_url, :public, :created_at, 
+        :updated_at, :comments, :comments_url);""", params)
+        db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,54 @@
 from .models import Gist
+from datetime import datetime
+
+'''
+Your search_gists method should take a db_connection parameter (the database connection)
+as well as two optional arguments
+
+* github_id
+* created_at
+
+If no parameter is provided, all the gists in the database should be returned. 
+If public_id or created_at parameters are provided, you should filter your SELECT 
+query based on them.
+'''
 
 def search_gists(db_connection, **kwargs):
-    pass
+    
+    search_query = "SELECT * FROM gists"
+    
+    if not kwargs:
+        cursor = db_connection.execute(search_query)
+    else:
+        args = ['github_id', 'created_at', 'updated_at']    
+        equality_ops = {'lte': '<=', 'lt': '<', 'gte': '>=', 'gt': '>'}
+        params = {}
+        addl_query = " WHERE "
+        
+        
+        # assume passed on: created_at__gte=d
+        for key, val in kwargs.items():
+            equality_operator = '='
+            param = key.split('__')
+            
+            if len(param) > 1:
+                equality_operator = equality_ops[param[1]]
+            
+            if param[0] in args:
+                params[param[0]] = kwargs[key]
+                print (kwargs[key])
+                
+                if type(val) == datetime:
+                    addl_query += "datetime({}) {} datetime(:{}) AND ".format(param[0], equality_operator, param[0])
+                else:
+                    addl_query += "{} {} :{} AND ".format(param[0], equality_operator, param[0]) 
+        
+        search_query+=addl_query[:-4]
+        
+        cursor = db_connection.execute(search_query, params)
+    
+    gists = []
+    for row in cursor.fetchall():
+        gists.append(Gist(row))
+    
+    return gists
